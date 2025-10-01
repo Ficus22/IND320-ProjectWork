831899e69436ceab44fb63979d68c5ee7b5d7d73
Ficus22 <130400795+Ficus22@users.noreply.github.com>
2025-10-01 14:06:58 +0200

    ajout de a propos

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -13,9 +13,9 @@ Bienvenue sur le dashboard d'analyse des données météorologiques de 2020.
 """)
 
 st.sidebar.title("Navigation")
-st.sidebar.page_link("app.py", label="Accueil")
-st.sidebar.page_link("pages/1_data_tables.py", label="Tableau de données")
-st.sidebar.page_link("pages/2_plots.py", label="Visualisations")
+#st.sidebar.page_link("app.py", label="Accueil")
+#st.sidebar.page_link("pages/1_data_tables.py", label="Tableau de données")
+#st.sidebar.page_link("pages/2_plots.py", label="Visualisations")
 st.sidebar.page_link("pages/3_about.py", label="À propos")
 
 st.image("https://kommunikasjon.ntb.no/data/images/00148/dee9f88e-8f69-42ee-aae0-ec20be9f2b7b.png", width=300) #NMBU image
